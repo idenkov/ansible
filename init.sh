ed18c527c551fc375c9e98b6b3d7e9cd564ace14
idenkov <ivan.denkov87@gmail.com>
2024-05-07 21:36:16 +0300

    Server update

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt install ansible ssh sshpass
-echo -e "\033[0;33mPlease run your role with "ansible-playbook {role}.yml -K -b"\033[0;33m"
+git submodule update --init
+echo -e "\033[0;33mPlease run your role with "ansible-playbook {role}.yml -K -k -b --ask-vault-password"\033[0;33m"
